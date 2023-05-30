3eb585c9ee1a548bf710ed87bb58e022a9141883
Andrew Mead <andrew@mead.io>
2023-05-30 14:59:51 -0400

    Add support for internationalized abbreviations

--- source/Number.php
+++ source/Number.php
@@ -24,7 +24,7 @@ class Number {
             if ( $number < $upper_range ) {
                 $decimals = $number < 1000 ? 0 : 1;
                 $result   = $number / $abbreviation_value;
-                $result   = number_format( $result, $decimals ) . $abbreviation;
+                $result   = number_format_i18n( $result, $decimals ) . $abbreviation;
 
                 // Strip out decimals that are 0 so 1.0T becomes 1T
                 $result = strpos( $result, '.0' ) === false ? $result : str_replace( '.0', '', $result );
@@ -34,6 +34,6 @@ class Number {
         }
 
         // Do nothing for numbers past the trillions
-        return (string) $number;
+        return number_format_i18n($number, 0);
     }
 }
--- tests/NumberTest.php
+++ tests/NumberTest.php
@@ -6,6 +6,13 @@ namespace Proper;
  * Tests for Number
  */
 class NumberTest extends \WP_UnitTestCase {
+    protected function setUp(): void {
+        switch_to_locale( 'en_US' );
+    }
+
+    public static function tearDownAfterClass(): void {
+        switch_to_locale( 'en_US' );
+    }
 
     public function test_small_numbers() {
         $actual   = Number::abbreviate( 1 );
@@ -56,9 +63,28 @@ class NumberTest extends \WP_UnitTestCase {
         $this->assertEquals( $expected, $actual );
     }
 
+    public function test_abbreviations_are_internationalized() {
+        switch_to_locale( 'de_DE' );
+        $actual   = Number::abbreviate( 1200 );
+        $expected = '1,2K';
+
+        $this->assertEquals( $expected, $actual );
+    }
+
     public function test_do_nothing_past_trillions() {
-        $actual   = Number::abbreviate(1000000000000000);
-        $expected = '1000000000000000';
+        // setlocale works with number_format which is from php
+        // switch_to_locale is wp and works with number_format_i18n which is also wordprsss
+
+        $actual   = Number::abbreviate( 1000000000000000 );
+        $expected = '1,000,000,000,000,000';
+
+        $this->assertEquals( $expected, $actual );
+    }
+
+    public function test_values_past_trillions_are_internationalized() {
+        switch_to_locale( 'de_DE' );
+        $actual   = Number::abbreviate( 1000000000000000 );
+        $expected = '1.000.000.000.000.000';
 
         $this->assertEquals( $expected, $actual );
     }
